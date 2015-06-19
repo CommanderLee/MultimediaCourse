beeb2704d4d8e4c969d8398544d9b3084aacf0a5
Zhen <lizhen11@mails.tsinghua.edu.cn>
2015-06-19 21:57:52 +0800

    Finish Exp1

--- ImageEncodeDecode/Matlab/exp1.m
+++ ImageEncodeDecode/Matlab/exp1.m
@@ -17,9 +17,9 @@ imgDCT1D = zeros(row, col);
 
 tic;
 keepCol = col / 16;
-markMat1D = [ ones(1, keepCol) zeros(1, col - keepCol) ];
+maskMat1D = [ ones(1, keepCol) zeros(1, col - keepCol) ];
 for r=1:row
-    imgDCT1D(r,:) = dct(myImgGray(r,:)) .* markMat1D;
+    imgDCT1D(r,:) = dct(myImgGray(r,:)) .* maskMat1D;
 end 
 toc;
 % figure, imshow(imgDCT1D);
@@ -29,7 +29,7 @@ imgIDCT1D = zeros(row, col);
 for r=1:row
     imgIDCT1D(r,:) = idct(imgDCT1D(r,:));
 end 
-% figure, imshow(imgIDCT1D);
+figure, imshow(imgIDCT1D);
 
 MSE = getMSE(myImgGray, imgIDCT1D);
 PSNR = 10 * log10(255^2 / MSE);
@@ -40,23 +40,27 @@ fprintf('1D-Keep %d coef: MSE:%f, PSNR:%f\n', keepCol, MSE, PSNR);
 % 2-D DCT.
 tic;
 % Keep [keepNum] DCT coef on the top-left corner of the DCT Matrix
-keepNum = row * col / 64;
+keepNum = row * col / 16;
 % The triangle on the top-left corner
-triangleLen = floor(sqrt(keepNum * 2)) - 1;
+triangleLen = floor(sqrt(keepNum * 2));
 onesRemain = keepNum - triangleLen * (triangleLen + 1) / 2;
+if onesRemain < 0
+    triangleLen = triangleLen - 1;
+    onesRemain = keepNum - triangleLen * (triangleLen + 1) / 2;
+end
 fprintf('Triangle Len:%d, ones remain:%d\n', triangleLen, onesRemain);
-markMat2D = zeros(row, col);
-% Marking the top-left corner of Mark Matrix
+maskMat2D = zeros(row, col);
+% masking the top-left corner of mask Matrix
 for r=1:onesRemain
-    markMat2D(r, 1:triangleLen+1) = ones(1, triangleLen+1);
+    maskMat2D(r, 1:triangleLen+1) = ones(1, triangleLen+1);
     triangleLen = triangleLen - 1;
 end
 for r=(onesRemain+1):row
-    markMat2D(r, 1:triangleLen) = ones(1, triangleLen);
+    maskMat2D(r, 1:triangleLen) = ones(1, triangleLen);
     triangleLen = triangleLen - 1;
 end
 
-imgDCT2D = dct2(myImgGray) .* markMat2D;
+imgDCT2D = dct2(myImgGray) .* maskMat2D;
 toc;
 imgIDCT2D = idct2(imgDCT2D);
 figure, imshow(imgIDCT2D);
@@ -70,16 +74,40 @@ fprintf('2D-Keep %d coef: MSE:%f, PSNR:%f\n', keepNum, MSE, PSNR);
 % 8*8 DCT
 tic;
 nSize = 8;
+
+% Keep [keepNum] DCT coef on the top-left corner of the DCT Matrix Block
+keepNum = nSize * nSize / 16;
+% The triangle on the top-left corner
+triangleLen = floor(sqrt(keepNum * 2));
+onesRemain = keepNum - triangleLen * (triangleLen + 1) / 2;
+if onesRemain < 0
+    triangleLen = triangleLen - 1;
+    onesRemain = keepNum - triangleLen * (triangleLen + 1) / 2;
+end
+fprintf('Triangle Len:%d, ones remain:%d\n', triangleLen, onesRemain);
+maskMat2DBlock = zeros(nSize, nSize);
+% masking the top-left corner of mask Matrix Block
+for r=1:onesRemain
+    maskMat2DBlock(r, 1:triangleLen+1) = ones(1, triangleLen+1);
+    triangleLen = triangleLen - 1;
+end
+for r=(onesRemain+1):nSize
+    maskMat2DBlock(r, 1:triangleLen) = ones(1, triangleLen);
+    triangleLen = triangleLen - 1;
+end
+
 dctMtx = dctmtx(nSize);
 dctMtx_ = dctMtx';
 funDct = @(block_struct) dctMtx * double(block_struct.data) * dctMtx_;
 imgDCTN = blockproc(myImgGray, [nSize nSize], funDct);
+funMask = @(block_struct) block_struct.data .* maskMat2DBlock;
+imgDCTNMasked = blockproc(imgDCTN, [nSize nSize], funMask);
 toc;
 
-% funIDct = @(block_struct) dctMtx_ * block_struct.data * dctMtx;
-% imgIDCTN = blockproc(imgDCTN, [nSize nSize], funIDct);
-% figure, imshow(imgIDCTN);
+funIDct = @(block_struct) dctMtx_ * block_struct.data * dctMtx;
+imgIDCTN = blockproc(imgDCTNMasked, [nSize nSize], funIDct);
+figure, imshow(imgIDCTN);
 
-% MSE = getMSE(myImgGray, imgIDCTN);
-% PSNR = 10 * log10(255^2 / MSE);
-% fprintf('8*8: MSE:%f, PSNR:%f\n', MSE, PSNR);
+MSE = getMSE(myImgGray, imgIDCTN);
+PSNR = 10 * log10(255^2 / MSE);
+fprintf('8*8: 2D-Keep %d, MSE:%f, PSNR:%f\n', keepNum, MSE, PSNR);
--- ImageEncodeDecode/Matlab/exp2.m
+++ ImageEncodeDecode/Matlab/exp2.m
@@ -1,27 +1,93 @@
 % Image Encoding and Decoding - Zhen Li, Tsinghua University
 
 % Load Image
-% myImg = im2double(imread('lena.bmp'));
-myImg = imread('lena.bmp');
+myImg = im2double(imread('lena.bmp'));
+% myImg = im2double(imread('COL.png'));
+% myImg = imread('lena.bmp');
 % imshow(myImg);
 
 % Convert to Gray-Scale
+% myImgGray = double(rgb2gray(myImg));
 myImgGray = rgb2gray(myImg);
-figure, imshow(myImgGray);
+% figure, imshow(myImgGray);
 imwrite(myImgGray, 'lena_gray.bmp');
+% imwrite(myImgGray, 'COL_gray.png');
 
 [row, col] = size(myImgGray);
 
 nSize = 8;
 dctMtx = dctmtx(nSize);
-dctMtx_ = dctMtx';
-funDct = @(block_struct) dctMtx * block_struct.data * dctMtx_;
-imgDCTN = blockproc(myImgGray, [nSize nSize], funDct);
 
-% funIDctQ = @(block_struct) dctMtx_ * block_struct.data * dctMtx;
-imgIDCTN = blockproc(imgDCTN, [nSize nSize], funIDctQ);
-figure, imshow(imgIDCTN);
+jpegQ = [16 11 10 16 24 40 51 61
+12 12 14 19 26 58 60 55
+14 13 16 24 40 57 69 56
+14 17 22 29 51 87 80 62
+18 22 37 56 68 109 103 77
+24 35 55 64 81 104 113 92
+649 64 78 87 103 121 120 101
+72 92 95 98 112 100 103 99];
 
-MSE = getMSE(myImgGray, imgIDCTN);
-PSNR = 10 * log10(255^2 / MSE);
+canonQ = [1 1 1 2 3 6 8 10
+    1 1 2 3 4 8 9 8
+    2 2 2 3 6 8 10 8
+    2 2 3 4 7 12 11 9
+    3 3 8 11 10 16 15 11
+    3 5 8 10 12 15 16 13
+    7 10 11 12 15 17 17 14
+    14 13 13 15 15 14 14 14];
 
+nikonQ = [2 1 1 2 3 5 6 7
+    1 1 2 2 3 7 7 7
+    2 2 2 3 5 7 8 7
+    2 2 3 3 6 10 10 7
+    2 3 4 7 8 13 12 9
+    3 4 7 8 10 12 14 11
+    6 8 9 10 12 15 14 12
+    9 11 11 12 13 12 12 12];
+
+% qMat = nikonQ;
+
+plotA = 0.1:0.05:2;
+plotPSNR = zeros(size(plotA, 1));
+plotIndex = 1;
+for a=plotA
+    qMat = jpegQ .* a;
+    
+    imgDCTNQ = zeros(row, col);
+    for r=1:8:row
+        for c=1:8:col
+            resultMat = dctMtx * (myImgGray(r:r+7,c:c+7) .* 255) * dctMtx';
+            imgDCTNQ(r:r+7,c:c+7) = round(resultMat ./ qMat);
+        end
+    end
+
+    % imgDCTNQ = blockproc(myImgGray, [nSize nSize], @funDctQ);
+    % dctMtx_ = dctMtx';
+    % funDct = @(block_struct) dctMtx * block_struct.data * dctMtx_;
+    % imgDCTN = blockproc(myImgGray, [nSize nSize], funDct);
+
+    % funIDctQ = @(block_struct) dctMtx' * block_struct.data * dctMtx;
+    % imgIDCTN = blockproc(imgDCTNQ, [nSize nSize], funIDctQ);
+    PSNRList = zeros(1, (row/8) * (col/8));
+    PSNRIndex = 1;
+    imgIDCTNQ = zeros(row, col);
+    for r=1:8:row
+        for c=1:8:col
+            resultMat = dctMtx' * (imgDCTNQ(r:r+7,c:c+7) .* qMat) * dctMtx;
+            imgIDCTNQ(r:r+7,c:c+7) = (resultMat) / 255.0;
+            MSE = getMSE(myImgGray(r:r+7,c:c+7), imgIDCTNQ(r:r+7,c:c+7));
+            PSNRList(PSNRIndex) = 10 * log10(255^2 / MSE);
+            PSNRIndex = PSNRIndex + 1;
+        end
+    end
+    meanPSNR = mean(PSNRList);
+    fprintf('PSNR Mean:%f\n', meanPSNR);
+    plotPSNR(plotIndex) = meanPSNR;
+    plotIndex = plotIndex + 1;
+%     figure, imshow(imgIDCTNQ);
+
+    MSE = getMSE(myImgGray, imgIDCTNQ);
+    PSNR = 10 * log10(255^2 / MSE);
+    fprintf('8*8: 2D-MSE:%f, PSNR:%f\n', MSE, PSNR);
+end
+figure, plot(plotA, plotPSNR);
